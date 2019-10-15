2f9e8d17b581a4d8a8ae6df9f97e88ed75f0c6e4
YamafukuKei <p104134k@mail.kyutech.jp>
2019-10-16 01:20:48 +0900

    Add to set symbolic link

---
+++ scripts/setup_link.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -eu
+
+DOT_FILES=(.rosrc .bashrc bin)
+
+for file in ${DOT_FILES[@]}
+do
+    if ls -a $HOME | grep $file > /dev/null; then
+        rm -rf $HOME/$file
+    fi
+
+    ln -s $HOME/dotfiles/$file $HOME/$file
+
+done
